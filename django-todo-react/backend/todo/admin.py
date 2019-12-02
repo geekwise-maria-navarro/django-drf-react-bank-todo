18ed5098d4eae02a59ec2c427f2c6a68c135c85c
geekwise-grace-liang <geekwise.grace.liang@gmail.com>
2019-12-02 22:21:54 +0000

    Set up the Backend, set up the APIs.

--- django-todo-react/backend/todo/admin.py
+++ django-todo-react/backend/todo/admin.py
@@ -2,5 +2,10 @@
 from __future__ import unicode_literals
 
 from django.contrib import admin
+from .models import Todo # add this
+
+class TodoAdmin(admin.ModelAdmin):  # add this
+    list_display = ('title', 'description', 'completed') # add this
 
 # Register your models here.
+admin.site.register(Todo, TodoAdmin) # add this
