75ee3e3e6ecde3c1c85d27aa922d05cc501f5c9d
Grace <grace@gracebook.local>
2019-12-27 13:45:34 -0800

    Edit: router register.

--- backend/app/main/urls.py
+++ backend/app/main/urls.py
@@ -3,13 +3,17 @@ from django.urls import path, include
 from rest_framework import routers
 
 from drf import views as drf_views
+from todo import views 
 
 router = routers.DefaultRouter()
 router.register('users', drf_views.user_viewset )
-
+router.register(r'todos', views.TodoView, 'todo')
 
 urlpatterns = [
     path('', admin.site.urls),
     path('api/', include(router.urls)),
     path('auth', include('rest_framework.urls', namespace='rest_framework')),
+
+    path('admin/', admin.site.urls),
+
 ]
