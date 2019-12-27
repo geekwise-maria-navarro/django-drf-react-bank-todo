0c4d1f5a3ae4489f41c7a097a5c104abf3955003
Grace <grace@gracebook.local>
2019-12-27 13:37:03 -0800

    Add: urls.py in main, is this correct folder?...

--- backend/app/main/settings.py
+++ backend/app/main/settings.py
@@ -25,12 +25,14 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # 'rest_framework',
     # 'drf',
+    'corsheaders',           
+    'rest_framework', 
     'todo',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -111,3 +113,7 @@ REST_FRAMEWORK = {
     'DEFAULT_PAGINATION_CLASS' : 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE' : 10
 }
+
+CORS_ORIGIN_WHITELIST = (
+         'https://localhost:3000',
+)
