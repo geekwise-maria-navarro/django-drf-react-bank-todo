7e29c1a0261c65459901fe22a2597a7acbbb06fa
Grace <grace@gracebook.local>
2019-12-30 14:30:46 -0800

    Update: add django_heroku settings.

--- backend/app/main/settings.py
+++ backend/app/main/settings.py
@@ -1,4 +1,5 @@
 import os
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -117,3 +118,5 @@ REST_FRAMEWORK = {
 CORS_ORIGIN_WHITELIST = (
          'https://localhost:3000',
 )
+
+django_heroku.settings(locals())
