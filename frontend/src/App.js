91fdfa74070b8274f7cf01e6afb17546959789cc
Grace <grace@gracebook.attlocal.net>
2020-01-02 13:55:26 -0800

    Update: app.js code.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from "react";
+    import Modal from "./components/Modal";
+
     const todoItems = [
       {
         id: 1,
@@ -29,10 +31,33 @@ import React, { Component } from "react";
       constructor(props) {
         super(props);
         this.state = {
+          modal: false,
           viewCompleted: false,
+          activeItem: {
+            title: "",
+            description: "",
+            completed: false
+          },
           todoList: todoItems
         };
       }
+      toggle = () => {
+        this.setState({ modal: !this.state.modal });
+      };
+      handleSubmit = item => {
+        this.toggle();
+        alert("save" + JSON.stringify(item));
+      };
+      handleDelete = item => {
+        alert("delete" + JSON.stringify(item));
+      };
+      createItem = () => {
+        const item = { title: "", description: "", completed: false };
+        this.setState({ activeItem: item, modal: !this.state.modal });
+      };
+      editItem = item => {
+        this.setState({ activeItem: item, modal: !this.state.modal });
+      };
       displayCompleted = status => {
         if (status) {
           return this.setState({ viewCompleted: true });
@@ -60,7 +85,7 @@ import React, { Component } from "react";
       renderItems = () => {
         const { viewCompleted } = this.state;
         const newItems = this.state.todoList.filter(
-          item => item.completed == viewCompleted
+          item => item.completed === viewCompleted
         );
         return newItems.map(item => (
           <li
@@ -76,8 +101,18 @@ import React, { Component } from "react";
               {item.title}
             </span>
             <span>
-              <button className="btn btn-secondary mr-2"> Edit </button>
-              <button className="btn btn-danger">Delete </button>
+              <button
+                onClick={() => this.editItem(item)}
+                className="btn btn-secondary mr-2"
+              >
+                Edit
+              </button>
+              <button
+                onClick={() => this.handleDelete(item)}
+                className="btn btn-danger"
+              >
+                Delete
+              </button>
             </span>
           </li>
         ));
@@ -90,7 +125,9 @@ import React, { Component } from "react";
               <div className="col-md-6 col-sm-10 mx-auto p-0">
                 <div className="card p-3">
                   <div className="">
-                    <button className="btn btn-primary">Add task</button>
+                    <button onClick={this.createItem} className="btn btn-primary">
+                      Add task
+                    </button>
                   </div>
                   {this.renderTabList()}
                   <ul className="list-group list-group-flush">
@@ -99,154 +136,268 @@ import React, { Component } from "react";
                 </div>
               </div>
             </div>
+            {this.state.modal ? (
+              <Modal
+                activeItem={this.state.activeItem}
+                toggle={this.toggle}
+                onSave={this.handleSubmit}
+              />
+            ) : null}
           </main>
         );
       }
     }
     export default App;
-    
 
 // import React, { Component } from "react";
-// import Modal from "./components/Modal";
-// import axios from "axios";
-
-// class App extends Component {
-//     constructor(props) {
-//     super(props);
-//     this.state = {
-//         viewCompleted: false,
-//         activeItem: {
-//         title: "",
-//         description: "",
+//     const todoItems = [
+//       {
+//         id: 1,
+//         title: "Go to Market",
+//         description: "Buy ingredients to prepare dinner",
+//         completed: true
+//       },
+//       {
+//         id: 2,
+//         title: "Study",
+//         description: "Read Algebra and History textbook for upcoming test",
 //         completed: false
-//         },
-//         todoList: []
-//     };
-//     }
-//     componentDidMount() {
-//     this.refreshList();
-//     }
-//     refreshList = () => {
-//     axios
-//         .get("https://django-drf-todo-grace.herokuapp.com/api/todos/")
-//         .then(res => this.setState({ todoList: res.data.results }))
-//         .catch(err => console.log(err));
-//     };
-//     displayCompleted = status => {
-//     if (status) {
-//         return this.setState({ viewCompleted: true });
-//     }
-//     return this.setState({ viewCompleted: false });
-//     };
-//     renderTabList = () => {
-//     return (
-//         <div className="my-5 tab-list">
-//         <span
-//             onClick={() => this.displayCompleted(true)}
-//             className={this.state.viewCompleted ? "active" : ""}
-//         >
-//             complete
-//         </span>
-//         <span
-//             onClick={() => this.displayCompleted(false)}
-//             className={this.state.viewCompleted ? "" : "active"}
-//         >
-//             Incomplete
-//         </span>
-//         </div>
-//     );
-//     };
-//     renderItems = () => {
-//     const { viewCompleted } = this.state;
-//     const newItems = this.state.todoList.filter(
-//         item => item.completed === viewCompleted
-//     );
-//     return newItems.map(item => (
-//         <li
-//         key={item.id}
-//         className="list-group-item d-flex justify-content-between align-items-center"
-//         >
-//         <span
-//             className={`todo-title mr-2 ${
-//             this.state.viewCompleted ? "completed-todo" : ""
-//             }`}
-//             title={item.description}
-//         >
-//             {item.title}
-//         </span>
-//         <span>
-//             <button
-//             onClick={() => this.editItem(item)}
-//             className="btn btn-secondary mr-2"
+//       },
+//       {
+//         id: 3,
+//         title: "Sally's books",
+//         description: "Go to library to rent sally's books",
+//         completed: true
+//       },
+//       {
+//         id: 4,
+//         title: "Article",
+//         description: "Write article on how to use django with react",
+//         completed: false
+//       }
+//     ];
+//     class App extends Component {
+//       constructor(props) {
+//         super(props);
+//         this.state = {
+//           viewCompleted: false,
+//           todoList: todoItems
+//         };
+//       }
+//       displayCompleted = status => {
+//         if (status) {
+//           return this.setState({ viewCompleted: true });
+//         }
+//         return this.setState({ viewCompleted: false });
+//       };
+//       renderTabList = () => {
+//         return (
+//           <div className="my-5 tab-list">
+//             <span
+//               onClick={() => this.displayCompleted(true)}
+//               className={this.state.viewCompleted ? "active" : ""}
 //             >
-//             {" "}
-//             Edit{" "}
-//             </button>
-//             <button
-//             onClick={() => this.handleDelete(item)}
-//             className="btn btn-danger"
+//               complete
+//             </span>
+//             <span
+//               onClick={() => this.displayCompleted(false)}
+//               className={this.state.viewCompleted ? "" : "active"}
 //             >
-//             Delete{" "}
-//             </button>
-//         </span>
-//         </li>
-//     ));
-//     };
-//     toggle = () => {
-//     this.setState({ modal: !this.state.modal });
-//     };
-//     handleSubmit = item => {
-//     this.toggle();
-//     if (item.id) {
-//         axios
-//         .put(`https://django-drf-todo-grace.herokuapp.com/api/todos/`, item)
-//         .then(res => this.refreshList());
-//         return;
-//     }
-//     axios
-//         .post("https://django-drf-todo-grace.herokuapp.com/api/todos/", item)
-//         .then(res => this.refreshList());
-//     };
-//     handleDelete = item => {
-//     axios
-//         .delete(`https://django-drf-todo-grace.herokuapp.com/api/todos/${item.id}`)
-//         .then(res => this.refreshList());
-//     };
-//     createItem = () => {
-//     const item = { title: "", description: "", completed: false };
-//     this.setState({ activeItem: item, modal: !this.state.modal });
-//     };
-//     editItem = item => {
-//     this.setState({ activeItem: item, modal: !this.state.modal });
-//     };
-//     render() {
-//     return (
-//         <main className="content">
-//         <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
-//         <div className="row ">
-//             <div className="col-md-6 col-sm-10 mx-auto p-0">
-//             <div className="card p-3">
-//                 <div className="">
-//                 <button onClick={this.createItem} className="btn btn-primary">
-//                     Add task
-//                 </button>
+//               Incomplete
+//             </span>
+//           </div>
+//         );
+//       };
+//       renderItems = () => {
+//         const { viewCompleted } = this.state;
+//         const newItems = this.state.todoList.filter(
+//           item => item.completed == viewCompleted
+//         );
+//         return newItems.map(item => (
+//           <li
+//             key={item.id}
+//             className="list-group-item d-flex justify-content-between align-items-center"
+//           >
+//             <span
+//               className={`todo-title mr-2 ${
+//                 this.state.viewCompleted ? "completed-todo" : ""
+//               }`}
+//               title={item.description}
+//             >
+//               {item.title}
+//             </span>
+//             <span>
+//               <button className="btn btn-secondary mr-2"> Edit </button>
+//               <button className="btn btn-danger">Delete </button>
+//             </span>
+//           </li>
+//         ));
+//       };
+//       render() {
+//         return (
+//           <main className="content">
+//             <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+//             <div className="row ">
+//               <div className="col-md-6 col-sm-10 mx-auto p-0">
+//                 <div className="card p-3">
+//                   <div className="">
+//                     <button className="btn btn-primary">Add task</button>
+//                   </div>
+//                   {this.renderTabList()}
+//                   <ul className="list-group list-group-flush">
+//                     {this.renderItems()}
+//                   </ul>
 //                 </div>
-//                 {this.renderTabList()}
-//                 <ul className="list-group list-group-flush">
-//                 {this.renderItems()}
-//                 </ul>
+//               </div>
 //             </div>
-//             </div>
-//         </div>
-//         {this.state.modal ? (
-//             <Modal
-//             activeItem={this.state.activeItem}
-//             toggle={this.toggle}
-//             onSave={this.handleSubmit}
-//             />
-//         ) : null}
-//         </main>
-//     );
+//           </main>
+//         );
+//       }
 //     }
-// }
-// export default App;
+//     export default App;
+    
+
+// // import React, { Component } from "react";
+// // import Modal from "./components/Modal";
+// // import axios from "axios";
+
+// // class App extends Component {
+// //     constructor(props) {
+// //     super(props);
+// //     this.state = {
+// //         viewCompleted: false,
+// //         activeItem: {
+// //         title: "",
+// //         description: "",
+// //         completed: false
+// //         },
+// //         todoList: []
+// //     };
+// //     }
+// //     componentDidMount() {
+// //     this.refreshList();
+// //     }
+// //     refreshList = () => {
+// //     axios
+// //         .get("https://django-drf-todo-grace.herokuapp.com/api/todos/")
+// //         .then(res => this.setState({ todoList: res.data.results }))
+// //         .catch(err => console.log(err));
+// //     };
+// //     displayCompleted = status => {
+// //     if (status) {
+// //         return this.setState({ viewCompleted: true });
+// //     }
+// //     return this.setState({ viewCompleted: false });
+// //     };
+// //     renderTabList = () => {
+// //     return (
+// //         <div className="my-5 tab-list">
+// //         <span
+// //             onClick={() => this.displayCompleted(true)}
+// //             className={this.state.viewCompleted ? "active" : ""}
+// //         >
+// //             complete
+// //         </span>
+// //         <span
+// //             onClick={() => this.displayCompleted(false)}
+// //             className={this.state.viewCompleted ? "" : "active"}
+// //         >
+// //             Incomplete
+// //         </span>
+// //         </div>
+// //     );
+// //     };
+// //     renderItems = () => {
+// //     const { viewCompleted } = this.state;
+// //     const newItems = this.state.todoList.filter(
+// //         item => item.completed === viewCompleted
+// //     );
+// //     return newItems.map(item => (
+// //         <li
+// //         key={item.id}
+// //         className="list-group-item d-flex justify-content-between align-items-center"
+// //         >
+// //         <span
+// //             className={`todo-title mr-2 ${
+// //             this.state.viewCompleted ? "completed-todo" : ""
+// //             }`}
+// //             title={item.description}
+// //         >
+// //             {item.title}
+// //         </span>
+// //         <span>
+// //             <button
+// //             onClick={() => this.editItem(item)}
+// //             className="btn btn-secondary mr-2"
+// //             >
+// //             {" "}
+// //             Edit{" "}
+// //             </button>
+// //             <button
+// //             onClick={() => this.handleDelete(item)}
+// //             className="btn btn-danger"
+// //             >
+// //             Delete{" "}
+// //             </button>
+// //         </span>
+// //         </li>
+// //     ));
+// //     };
+// //     toggle = () => {
+// //     this.setState({ modal: !this.state.modal });
+// //     };
+// //     handleSubmit = item => {
+// //     this.toggle();
+// //     if (item.id) {
+// //         axios
+// //         .put(`https://django-drf-todo-grace.herokuapp.com/api/todos/`, item)
+// //         .then(res => this.refreshList());
+// //         return;
+// //     }
+// //     axios
+// //         .post("https://django-drf-todo-grace.herokuapp.com/api/todos/", item)
+// //         .then(res => this.refreshList());
+// //     };
+// //     handleDelete = item => {
+// //     axios
+// //         .delete(`https://django-drf-todo-grace.herokuapp.com/api/todos/${item.id}`)
+// //         .then(res => this.refreshList());
+// //     };
+// //     createItem = () => {
+// //     const item = { title: "", description: "", completed: false };
+// //     this.setState({ activeItem: item, modal: !this.state.modal });
+// //     };
+// //     editItem = item => {
+// //     this.setState({ activeItem: item, modal: !this.state.modal });
+// //     };
+// //     render() {
+// //     return (
+// //         <main className="content">
+// //         <h1 className="text-white text-uppercase text-center my-4">Todo app</h1>
+// //         <div className="row ">
+// //             <div className="col-md-6 col-sm-10 mx-auto p-0">
+// //             <div className="card p-3">
+// //                 <div className="">
+// //                 <button onClick={this.createItem} className="btn btn-primary">
+// //                     Add task
+// //                 </button>
+// //                 </div>
+// //                 {this.renderTabList()}
+// //                 <ul className="list-group list-group-flush">
+// //                 {this.renderItems()}
+// //                 </ul>
+// //             </div>
+// //             </div>
+// //         </div>
+// //         {this.state.modal ? (
+// //             <Modal
+// //             activeItem={this.state.activeItem}
+// //             toggle={this.toggle}
+// //             onSave={this.handleSubmit}
+// //             />
+// //         ) : null}
+// //         </main>
+// //     );
+// //     }
+// // }
+// // export default App;
