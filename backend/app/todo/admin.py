df7d2b9357d0da37cef432c40b22dbe6ce65f14a
Grace <grace@gracebook.local>
2020-01-14 14:03:55 -0800

    Update: created models and registered them.

--- backend/app/todo/admin.py
+++ backend/app/todo/admin.py
@@ -1,11 +1,19 @@
 from django.contrib import admin
-from .models import Todo, Branch
+from .models import Todo, Branch, Customer
 
 # Register your models here.
 class TodoAdmin(admin.ModelAdmin):
     list_display = ('title', 'description', 'completed')
 
+class BranchAdmin(admin.ModelAdmin):
+    list_display = ('bank_name', 'location')
+
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('customer_name', 'phone_number', 'email', 'address')
+
     # Register your models here.
 admin.site.register(Todo, TodoAdmin) #make sure no tabs for line
 
-admin.site.register(Branch)
+admin.site.register(Branch, BranchAdmin)
+
+admin.site.register(Customer, CustomerAdmin)
