ec3e683c4867ebdbd7ab842c450321d7525cff65
Grace <grace@gracebook.local>
2020-01-16 13:14:50 -0800

    Update: allowed hosts in settingspy.

--- backend/app/main/settings.py
+++ backend/app/main/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = '(d3!3p2x4w)j@6q$!0to8oudbyo=6e(7k=14_+o59qk!+1823*'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['bank-frontend.herokuapp.com']
 
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': ('knox.auth.TokenAuthentication',),
